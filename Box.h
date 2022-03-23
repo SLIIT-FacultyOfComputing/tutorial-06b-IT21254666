00b36952443df8cd40ab829ba9ceef5fb11ba2db
Nethmi De Silva <99467345+IT21254666@users.noreply.github.com>
2022-03-23 03:26:20 +0000

    Completed both exercises.

--- Box.h
+++ Box.h
@@ -1,11 +1,17 @@
+
 class Box {
     private:
        int length;
        int width;
        int height;
     public:
+       void setLength(int l);
+       void setWidth(int w);
+       void setHeight(int h);
+       int getLength();
+       int getWidth();
+       int getHeight();
        // write prototypes of setters for length, width and height
        // write prototypes of getters for length, width and height
- 
        int calcVolume();
 };
